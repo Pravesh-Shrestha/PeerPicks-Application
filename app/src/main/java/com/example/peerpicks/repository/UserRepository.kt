34d246f67b2e50ebdff22b7d7ef34f34fcdebfe2
Pravesh <stharavesh12@gmail.com>
2025-01-09 13:13:41 +0545

    Repo

--- app/src/main/java/com/example/peerpicks/repository/UserRepository.kt
+++ app/src/main/java/com/example/peerpicks/repository/UserRepository.kt
@@ -1,10 +1,14 @@
 package com.example.peerpicks.repository
 
+import com.example.peerpicks.model.UserModel
+import com.google.firebase.auth.FirebaseUser
+
 interface UserRepository {
     //{
 //    "success":true
 //    "message" : "Login sucess"
     // }
+
     fun login(email:String,password:String,callback:(Boolean,String)->Unit)
 
 //    {
@@ -15,7 +19,10 @@ interface UserRepository {
 //        "user Id": "12345pkp12345"
 //    }
     fun signup(email: String,password: String,callback: (Boolean, String, String) -> Unit)
-    fun addUserToDatabase()
-    fun forgetPassword()
-    fun getCurrentPaasword()
+
+    fun addUserToDatabase(userId:String,userModel: UserModel,callback: (Boolean, String) -> Unit)
+
+    fun forgetPassword(email:String,callback: (Boolean, String) -> Unit)
+
+    fun getCurrentUser():FirebaseUser?
 }
