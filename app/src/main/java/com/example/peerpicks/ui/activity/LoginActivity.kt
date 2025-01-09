fd234ca0e1189ce2d56f828a65b4d768fc5c7497
Pravesh <stharavesh12@gmail.com>
2025-01-09 14:34:10 +0545

    MVVM integrated

--- app/src/main/java/com/example/peerpicks/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/peerpicks/ui/activity/LoginActivity.kt
@@ -2,22 +2,52 @@ package com.example.peerpicks.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.peerpicks.R
 import com.example.peerpicks.databinding.ActivityLoginBinding
+import com.example.peerpicks.repository.UserRepositoryImpl
+import com.example.peerpicks.viewModel.UserViewModel
 
 class LoginActivity : AppCompatActivity() {
+
     lateinit var binding:ActivityLoginBinding
+    lateinit var userViewModel:UserViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding=ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var repo= UserRepositoryImpl()
+        userViewModel= UserViewModel(repo)
+
+        binding.btnLogin.setOnClickListener{
+            var email=binding.editEmail.text.toString()
+            var password=binding.editPassword.text.toString()
+
+
+            userViewModel.login(email,password){
+                    success,message ->
+                if (success){
+                    var intent=Intent(this@LoginActivity,
+                        NavigationActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(
+                        this@LoginActivity,
+                        message,
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                }
+            }
+        }
         binding.btnSignupnavigate.setOnClickListener{
+
             val intent=Intent(this@LoginActivity,
                 RegisterActivity::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/peerpicks/ui/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/peerpicks/ui/activity/RegisterActivity.kt
@@ -9,13 +9,15 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.peerpicks.R
 import com.example.peerpicks.databinding.ActivityRegisterBinding
 import com.example.peerpicks.model.UserModel
+import com.example.peerpicks.repository.UserRepositoryImpl
+import com.example.peerpicks.viewModel.UserViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var binding:ActivityRegisterBinding
-
+    lateinit var userViewModel: UserViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +26,10 @@ class RegisterActivity : AppCompatActivity() {
         binding=ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        auth=FirebaseAuth.getInstance()
+//        auth=FirebaseAuth.getInstance()
+
+        var repo=UserRepositoryImpl()
+        userViewModel= UserViewModel(repo)
 
         binding.signUp.setOnClickListener{
             var email=binding.registerEmail.text.toString()
@@ -34,23 +39,72 @@ class RegisterActivity : AppCompatActivity() {
             var address=binding.registerAddress.text.toString()
             var contact=binding.registerContact.text.toString()
 
-            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener{
-                if(it.isSuccessful){
-                    var userId=auth.currentUser?.uid
-
+            userViewModel.signup(email,password){
+                success,message,userId->
+                if(success){
                     var userModel=UserModel(
                         userId.toString(),
                         firstName, lastName,
                         address, contact, email
                     )
 
-                } else{
-                    Toast.makeText(this@RegisterActivity,
-                        it.exception?.message.toString(),
-                        Toast.LENGTH_LONG).show()
+                    userViewModel.addUserToDatabase(userId, userModel){
+                        success,message ->
+                        if (success){
+                            Toast.makeText(
+                                this@RegisterActivity,
+                                "User Added Success",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }else{
+                            Toast.makeText(this@RegisterActivity,
+                                message,Toast.LENGTH_LONG)
+                                .show()
+                        }
                     }
+                }else{
+                    Toast.makeText(this@RegisterActivity,
+                        message,Toast.LENGTH_LONG)
+                        .show()
+
+                }
 
             }
+
+
+
+
+//            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener{
+//                if(it.isSuccessful){
+//                    var userId=auth.currentUser?.uid
+//
+//                    var userModel=UserModel(
+//                        userId.toString(),
+//                        firstName, lastName,
+//                        address, contact, email
+//                    )
+//                    ref.child(userId.toString()).setValue(userModel).addOnCompleteListener {
+//                        if (it.isSuccessful) {
+//                            Toast.makeText(
+//                                this@RegisterActivity,
+//                                "Register Success",
+//                                Toast.LENGTH_LONG
+//                            ).show()
+//                        } else {
+//                            Toast.makeText(
+//                                this@RegisterActivity,
+//                                it.exception?.message.toString(),
+//                                Toast.LENGTH_LONG).show()
+//                        }
+//                    }
+//
+//                } else{
+//                    Toast.makeText(this@RegisterActivity,
+//                        it.exception?.message.toString(),
+//                        Toast.LENGTH_LONG).show()
+//                    }
+//
+//            }
 
         }
 
