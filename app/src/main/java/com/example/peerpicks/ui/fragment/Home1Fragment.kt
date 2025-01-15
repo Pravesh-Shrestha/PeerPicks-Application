276ac99661da40b33e57bd9c487765761651d966
Pravesh <stharavesh12@gmail.com>
2025-01-15 13:36:27 +0545

    TODO Removed

--- app/src/main/java/com/example/peerpicks/ui/fragment/Home1Fragment.kt
+++ app/src/main/java/com/example/peerpicks/ui/fragment/Home1Fragment.kt
@@ -18,7 +18,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Home1Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -39,16 +39,7 @@ class Home1Fragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Home1Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
+
         fun newInstance(param1: String, param2: String) =
             Home1Fragment().apply {
                 arguments = Bundle().apply {
