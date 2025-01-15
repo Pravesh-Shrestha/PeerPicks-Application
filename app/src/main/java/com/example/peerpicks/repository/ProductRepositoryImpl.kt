143c1f0f5b00c3fbae38ad635fdcbed20015b369
Pravesh <stharavesh12@gmail.com>
2025-01-15 13:33:45 +0545

    Instance Created

--- app/src/main/java/com/example/peerpicks/repository/ProductRepositoryImpl.kt
+++ app/src/main/java/com/example/peerpicks/repository/ProductRepositoryImpl.kt
@@ -1,8 +1,13 @@
 package com.example.peerpicks.repository
 
 import com.example.peerpicks.model.ProductModel
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ProductRepositoryImpl : ProductRepository {
+    val database:FirebaseDatabase= FirebaseDatabase.getInstance()
+    val reference:DatabaseReference=database.reference.child("Products")
+
     override fun addProduct(productModel: ProductModel, callback: (Boolean, String) -> Unit) {
 
     }
